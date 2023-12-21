32e94a013b41339490b5e034495b18d589052c44
harivilasp <harivilasp@gmail.com>
2023-12-21 18:58:12 -0500

    ready-what-wanted

--- content.js
+++ content.js
@@ -1,7 +1,5 @@
 if (
-  !confirm(
-    "Are you sure you are opening YouTube for study and won't waste time?"
-  )
+  !confirm("Are you sure you are opening for purpose and won't waste time?")
 ) {
   window.location.href = "about:blank";
 }
